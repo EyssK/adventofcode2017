3ac414f6cc59d4698044f94b4cfa1f6161460e57
Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
2017-12-14 18:38:41 +0100

    modification of Day10 for Day14

--- Day10.py
+++ Day10.py
@@ -2,7 +2,20 @@ from itertools import cycle
 from functools import reduce
 
 
-def knotHash(in_list, cur_pos, length):
+def computeSparseHash(inputs, seq, iter):
+    cur_pos = 0
+    skip = 0
+    res_len = len(seq)
+
+    for idx, length in  enumerate(cycle(inputs)):
+        if idx / len(inputs) >= iter:
+            break
+        pinchAndTwist(seq, cur_pos, length)
+        cur_pos = (cur_pos + length + skip) % res_len
+        skip += 1
+
+
+def pinchAndTwist(in_list, cur_pos, length):
     sub_list = list()
     for idx, value in enumerate(cycle(in_list)):
         if idx < cur_pos:
@@ -18,9 +31,27 @@ def knotHash(in_list, cur_pos, length):
             continue
         if idx >= cur_pos + length:
             break
-        in_list[idx % res_len] = sub_list[nex_list_idx]
+        in_list[idx % len(in_list)] = sub_list[nex_list_idx]
         nex_list_idx += 1
-    return in_list
+
+
+def knotHash(line):
+    inputs = list()
+    for char in line:
+        inputs.append(ord(char))
+    inputs += [17, 31, 73, 47, 23]
+
+    sparse_hash = [x for x in range(256)]
+    computeSparseHash(inputs, sparse_hash, 64)
+
+    dense_hash = list()
+    for group in range(16):
+        dense_hash.append(reduce(lambda x, y: x ^ y, sparse_hash[group*16:(group+1)*16]))
+
+    khash = ""
+    for val in dense_hash:
+        khash += hex(val)[2:]
+    return khash
 
 
 if __name__ == "__main__":
@@ -30,20 +61,12 @@ if __name__ == "__main__":
             lines.append(line[:-1])
 
     inputs = [int(x) for x in lines[0].split(',')]
-    res_list = [x for x in range(256)]
 
     # test
     # res_list = [0, 1, 2, 3, 4]
     # inputs = [3, 4, 1, 5]
-
-    cur_pos = 0
-    skip = 0
-    res_len = len(res_list)
-
-    for length in inputs:
-        res_list = knotHash(res_list, cur_pos, length)
-        cur_pos = (cur_pos + length + skip) % res_len
-        skip += 1
+    res_list = [x for x in range(256)]
+    computeSparseHash(inputs, res_list, 1)
     print(res_list[0] * res_list[1])
 
     # Part 2
@@ -54,25 +77,5 @@ if __name__ == "__main__":
     # lines[0] = "1,2,3"
     # lines[0] = "1,2,4"
 
-    inputs = list()
-    for char in lines[0]:
-        inputs.append(ord(char))
-    inputs += [17, 31, 73, 47, 23]
-    sparse_hash = [x for x in range(256)]
-    cur_pos = 0
-    skip = 0
-
-    for rd in range(64):
-        for length in inputs:
-            res_list = knotHash(sparse_hash, cur_pos, length)
-            cur_pos = (cur_pos + length + skip) % res_len
-            skip += 1
-
-    dense_hash = list()
-    for group in range(16):
-        dense_hash.append(reduce(lambda x, y: x ^ y, sparse_hash[group*16:(group+1)*16]))
-
-    dense_string = ""
-    for val in dense_hash:
-        dense_string += hex(val)[2:]
-    print(dense_string)
+    khash = knotHash(lines[0])
+    print(hash)
