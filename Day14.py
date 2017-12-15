31890de4c7424dc6893ccfc38a1052989a877342
Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
2017-12-15 17:21:24 +0100

    Trying for Part2 of Day14 Something not OK Tested with soltuions found on reddit : same answer as mine for part 1 and 2 Maybe error in Day10

--- Day14.py
+++ Day14.py
@@ -1,5 +1,8 @@
 from Day10 import knotHash
-import encodings
+from itertools import product
+
+
+TABLE_SIZE = 128
 
 input = 'xlqgujun'
 
@@ -7,25 +10,64 @@ input = 'xlqgujun'
 # input = 'flqrgnkx'
 
 
-def bitcount(n):
-    count = 0
-    while n > 0:
-        if (n & 1 == 1): count += 1
-        n >>= 1
-    return count
+def get_neighbors(coord):
+    neighbors_list = list()
+    if coord[0] > 0:
+        neighbors_list.append((coord[0]-1, coord[1]))
+    if coord[0] < (TABLE_SIZE - 1):
+        neighbors_list.append((coord[0]+1, coord[1]))
+    if coord[1] > 0:
+        neighbors_list.append((coord[0], coord[1]-1))
+    if coord[1] < (TABLE_SIZE - 1):
+        neighbors_list.append((coord[0], coord[1]+1))
+    return neighbors_list
 
+
+def check_neighbors(table, coord):
+    """ return list of neighbors in the group """
+    neighbors_in_list = list()
+    for neighbor in get_neighbors(coord):
+        if table[neighbor[0]][neighbor[1]]:
+            neighbors_in_list.append(neighbor)
+    return neighbors_in_list
 
 if __name__ == "__main__":
 
-    defrag_table = list()
+    defrag_list = list()
+    defrag_table = [[0 for x in range(128)] for y in range(128)]
     for row in range(128):
-        defrag_table.append(knotHash(input + "-" + str(row)))
+        khash = knotHash(input + "-" + str(row))
+        defrag_list.append(khash)
+        khash_val = int(khash, 16)
+        for col in range(128):
+            defrag_table[col][row] = (khash_val & (1 << col)) >> col
 
-    sum_used = 0
-    for row in defrag_table:
-        sum_used += bitcount(int(row, 16))
-    print('part1: ', sum_used)
+    total_nb_of_region = sum(map(sum, defrag_table))
+    print('part1: ', total_nb_of_region)
 
-    # table = dict{ row: [list of groups in this line]}
-    # group = dict{ min, max: , connected: [ list of groups ]
+    # part 2
+    # build group lists
+    groups_list = list()
+    group_nb = 0
+    for col, row in product(range(TABLE_SIZE), range(TABLE_SIZE)):
+        if (col, row) in groups_list:
+            # this coord already in a group
+            continue
+        if defrag_table[col][row]:
+            # explore new group
+            group_nb += 1
+            remain = [(col,row)]
+            while len(remain):
+                if remain[0] in groups_list:
+                    # his neighbors had already been checked
+                    remain = remain[1:]
+                else:
+                    remain += check_neighbors(defrag_table, remain[0])
+                    groups_list.append(remain[0])
+                    remain = remain[1:]
 
+        else:
+            # defrag_table = 0
+            continue
+
+    print("part2: ", group_nb)
